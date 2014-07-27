6c6ebbda1d1fd2f16741cd1d8b6cd54c4f3c650a
Radoslaw Scheibinger <rscheibinger@ebay.com>
2014-07-27 12:00:09 +0100

    Configured jekyll with grunt and livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,181 @@
 module.exports = function (grunt) {
+    // Show elapsed time after tasks run
+    require('time-grunt')(grunt);
+    // Load all Grunt tasks
+    require('load-grunt-tasks')(grunt);
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        resourcePath: "app",
+        yeoman: {
+            app: 'app',
+            dist: 'dist'
+        },
         less: {
             development: {
                 options: {
-                    paths: ["<%= resourcePath %>/styles"]
+                    paths: ["<%= yeoman.app %>/styles"]
                 },
                 files: {
-                    "<%= resourcePath %>/styles/main.css": "<%= resourcePath %>/styles/main.less",
+                    "<%= yeoman.app %>/styles/main.css": "<%= yeoman.app %>/styles/main.less",
+                }
+            }
+        },
+        jekyll: {
+            options: {
+                bundleExec: true,
+                config: '_config.yml,_config.build.yml',
+                src: '<%= yeoman.app %>'
+            },
+            dist: {
+                options: {
+                    dest: '<%= yeoman.dist %>',
+                }
+            },
+            server: {
+                options: {
+                    config: '_config.yml',
+                    dest: '.jekyll'
+                }
+            },
+            check: {
+                options: {
+                    doctor: true
                 }
             }
         },
         watch: {
             less: {
-                files: "<%= resourcePath %>/styles/**/*.less",
+                files: "<%= yeoman.app %>/styles/**/*.less",
                 tasks: ['less']
+            },
+            jekyll: {
+                files: [
+                    '<%= yeoman.app %>/**/*.{html,yml,md,mkd,markdown}',
+                    '!<%= yeoman.app %>/_bower_components/**/*'
+                ],
+                tasks: ['jekyll:server']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '.jekyll/**/*.html',
+                    '{.tmp,<%= yeoman.app %>}/styles/**/*.css',
+                    '{.tmp,<%= yeoman.app %>}/<%= scripts %>/**/*.js',
+                    '<%= yeoman.app %>/images/**/*.{gif,jpg,jpeg,png,svg,webp}'
+                ]
             }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '.tmp',
+                        '.jekyll',
+                        '<%= yeoman.app %>'
+                    ]
+                }
+            },
+            dist: {
+                options: {
+                    open: true,
+                    base: [
+                        '<%= yeoman.dist %>'
+                    ]
+                }
+            },
+            test: {
+                options: {
+                    base: [
+                        '.tmp',
+                        '.jekyll',
+                        'test',
+                        '<%= yeoman.app %>'
+                    ]
+                }
+            }
+        },
+        clean: {
+            dist: {
+                files: [
+                    {
+                        dot: true,
+                        src: [
+                            '<%= yeoman.dist %>/*',
+                            // Running Jekyll also cleans the target directory.  Exclude any
+                            // non-standard `keep_files` here (e.g., the generated files
+                            // directory from Jekyll Picture Tag).
+                            '!<%= yeoman.dist %>/.git*'
+                        ]
+                    }
+                ]
+            },
+            server: [
+                '.tmp',
+                '.jekyll'
+            ]
+        },
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: '<%= yeoman.app %>',
+                        src: [
+                            // Jekyll processes and moves HTML and text files.
+                            // Usemin moves CSS and javascript inside of Usemin blocks.
+                            // Copy moves asset files and directories.
+                            'images/**/*',
+                            'styles/**/*',
+                            'scripts/**/*',
+                            // Like Jekyll, exclude files & folders prefixed with an underscore.
+                            '!**/_*{,/**}'
+                            // Explicitly add any files your site needs for distribution here.
+                            //'_bower_components/jquery/jquery.js',
+                            //'favicon.ico',
+                            //'apple-touch*.png'
+                        ],
+                        dest: '<%= yeoman.dist %>'
+                    }
+                ]
+            }
+        },
+
+        concurrent: {
+            server: [
+                'jekyll:server'
+            ],
+            dist: [
+                'copy:dist'
+            ]
         }
 
     });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
     // Default task(s).
     grunt.registerTask('default', ['less']);
+
+    // Define Tasks
+    grunt.registerTask('serve', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:server',
+            'concurrent:server',
+            'connect:livereload',
+            'watch'
+        ]);
+    });
 };
