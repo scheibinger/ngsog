3532d6ed9e1824dd138754f74292e6ec8a88103c
Radoslaw Scheibinger <scheibinger@gmail.com>
2014-07-28 23:26:34 +0100

    Built ngsog from commit bae1e3c on branch master

---
+++ en/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $.cookieBar(
+    {
+      message:"Informujemy, iż w naszym serwisie internetowym korzystamy z informacji zapisanych za pomocą plików cookies na urządzeniach końcowych użytkowników.",
+      acceptText:"Zrozumiałem i Wyrażam zgodę"
+    }
+  );
+});
