ba4107b78b99d822488ead242f5f991a5bf68d4f
Radoslaw Scheibinger <scheibinger@gmail.com>
2014-09-26 00:06:06 +0100

    making deploy silent

--- Gruntfile.js
+++ Gruntfile.js
@@ -167,7 +167,8 @@ module.exports = function (grunt) {
         dir: 'dist',
         commit: true,
         push: true,
-        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%',
+        silent: true
       },
       pages: {
         options: {
