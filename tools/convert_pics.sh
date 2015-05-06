3f9fb0c9d32bf336dd0a6b456876e359bd7bbb9d
Radoslaw Scheibinger <rscheibinger@ebay.com>
2015-05-06 20:31:03 +0100

    updated pictures for offer

--- tools/convert_pics.sh
+++ tools/convert_pics.sh
@@ -19,14 +19,15 @@ i=0;
 for file in $1*; do
 	((i++));
 	echo $file;
-	base=$(basename ${file})
-	imgPath="$albumPath/$base.jpg";
+	baseFileName=$(basename ${file})
+	filenameNoExtension="${baseFileName%.*}"
+	imgPath="$albumPath/$filenameNoExtension.jpg";
 	echo "creating $imgPath";
-	thumbPath="$albumPath/${base}_thumb.jpg";
+	thumbPath="$albumPath/${filenameNoExtension}_thumb.jpg";
 	convert $file -resize x900 $imgPath;
 	echo "creating $thumbPath";
 	#convert $file -resize x111 $thumbPath;
 	convert $file -resize "200x133^" -gravity center -crop 200x133+0+0 +repage $thumbPath;
-	echo "<a href=\"/images/ngsog/$2/$base.jpg\" title=\"NGS Oil & Gas 2012\"> <img src=\"/images/ngsog/$2/${base}_thumb.jpg\" alt=\"\" /> </a>"  >> "$albumPath/content.html";
+	echo "<a href=\"/images/ngsog/$2/$filenameNoExtension.jpg\"> <img src=\"/images/ngsog/$2/${filenameNoExtension}_thumb.jpg\" alt=\"${filenameNoExtension}\" /> </a>"  >> "$albumPath/content.html";
 done
 echo "</div><script type=\"text/javascript\">\$(function() {\$('#gallery a').lightBox();});</script>" >> "$albumPath/content.html";
